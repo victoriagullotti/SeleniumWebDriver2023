6cfe7d846db612dae2ac3b1084759e74d3b5e653
Victoria Gullotti <victoria.gullotti@navitas.com>
2023-11-28 14:35:03 +0800

    Add function text() examples.

---
+++ xpath_locators/03_functions_text.py
@@ -0,0 +1,5 @@
+# Get H5 element with text 'Create list of your favorite foods'
+h5_element_with_text = '//h5[text()="Create list of your favorite foods"]'
+
+# Get a link with text 'Selenium WebDriver with Java for beginners program'
+link_with_text_locator = '//a[text()="Selenium WebDriver with Java for beginners program"]'
