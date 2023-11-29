1014f332cfc069fc93e58e974f8b844db9482763
Victoria Gullotti <victoria.gullotti@navitas.com>
2023-11-30 07:31:22 +0800

    Add combine several locators examples.

---
+++ xpath_locators/08_combine_several_locators.py
@@ -0,0 +1,8 @@
+# Combine H3 and H5
+all_h3_and_h5 = '$x("//h2 | //h5")'
+
+# Get buttons and inputs inside row1
+buttons_and_inputs = '$x("//div[@id=\'row1\']/input | //div[@id=\'row1\']/button")'
+
+# All h3, h5 and p tags
+all_h3_h5_p = '$x("//h3 | //h5 | //p")'
