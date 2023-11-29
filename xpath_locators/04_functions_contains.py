e1a8f33571d038747f2103b20e49bc046a11ffd2
Victoria Gullotti <victoria.gullotti@navitas.com>
2023-11-29 14:48:10 +0800

    Add function contains() examples.

---
+++ xpath_locators/04_functions_contains.py
@@ -0,0 +1,5 @@
+# Get text element containing 'This page'
+p_element_with_text = '//p[contains(text(), "This page")]'
+
+# Start with
+p_element_with_text = '//p[starts-with(text(), "Row 2 doesn")]'
